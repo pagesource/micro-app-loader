e3160bca5914765d81b547947b5f89b811aca088
sahitikeesari <sahitikeesari@gmail.com>
2021-10-19 13:13:24 +0530

    feat(module-federation-config) Added config for module-federation

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   reactStrictMode: true,
+  webpack: (config, options) => {
+    const { ModuleFederationPlugin } = options.webpack.container;
+    config.plugins.push(
+      new ModuleFederationPlugin({
+        remotes: {
+          // example: microapp: "microapp@http://localhost:5000/home/dist/remoteEntry.js",
+        },
+        shared: [],
+      })
+    );
+
+    return config;
+  },
 }
